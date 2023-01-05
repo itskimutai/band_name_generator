f4129f492364209d4d7e7a232228a97644eb8e75
Daniel Kimutai <daniellkimutai@gmail.com>
2023-01-05 21:20:24 +0300

    version 1 _ band name generator

---
+++ main.py
@@ -0,0 +1,7 @@
+# #Band name generator application
+print("Welcome to the Band Name /  Business Generator")
+cityName = input("What's the name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+print("Your band/city name could be: " + cityName + petName)
+
+
